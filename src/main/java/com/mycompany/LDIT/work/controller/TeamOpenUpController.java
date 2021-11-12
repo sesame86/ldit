3f7fada51f92b25dd5686cdc6c6369f63b6593e3
yeeun kim <kyy8006@naver.com>
2021-11-12 20:12:15 +0900

    팀개설/수정 페이지 화면 구현

--- src/main/java/com/mycompany/LDIT/work/controller/TeamOpenUpController.java
+++ src/main/java/com/mycompany/LDIT/work/controller/TeamOpenUpController.java
@@ -1,8 +1,16 @@
 package com.mycompany.LDIT.work.controller;
 
+import java.util.Locale;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller("topen")
 public class TeamOpenUpController {
-
+	@RequestMapping(value = "/teamadd", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		return "team_add";
+	}
 }
