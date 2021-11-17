c2c6a3abe69539ad03e3bc8b3290e1164da74712
taerifu <neverlatelife@gmail.com>
2021-11-17 15:37:28 +0900

    json-lib-ext-spring 추가

--- src/main/java/com/mycompany/ldit/attendance/controller/AttendanceController.java
+++ src/main/java/com/mycompany/ldit/attendance/controller/AttendanceController.java
@@ -2,19 +2,25 @@ package com.mycompany.ldit.attendance.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AttendanceController {
 	
-	@RequestMapping(value="attcheck")
+	@RequestMapping(value="attcheck", method = RequestMethod.GET)
 	public ModelAndView attMainMethod(ModelAndView mv) {
 		mv.setViewName("attendance/attcheck");
 		
 		return mv;
 	}
 	
-	
+	@RequestMapping(value="checkin", method = RequestMethod.POST)
+	@ResponseBody
+	public void checkinMethod() {
+		
+	}
 	
 
 }
