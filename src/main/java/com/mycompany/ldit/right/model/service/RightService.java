f5dc0b9cbcc23a52197bab8ea0009e95e40a5b6a
sualocin92 <sualocin92@naver.com>
2021-11-24 18:34:45 +0900

    권한관리 기능 완료, 프로젝트생성 진행중, vo변수명 수정 및 폴더 변경

--- src/main/java/com/mycompany/ldit/right/model/service/RightService.java
+++ src/main/java/com/mycompany/ldit/right/model/service/RightService.java
@@ -2,8 +2,10 @@ package com.mycompany.ldit.right.model.service;
 
 import java.util.List;
 
+import com.mycompany.ldit.right.model.vo.Right;
 import com.mycompany.ldit.staff.model.vo.Staff;
 
 public interface RightService {
 	public List<Staff> getStaffList(Staff vo) throws Exception;
+	public int insertPm(int rightSno) throws Exception;
 }
